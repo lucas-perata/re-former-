72f39e5ea35d3b335482a8141c32133c6ad3c3f1
lucas-perata <contacto.perata@gmail.com>
2022-05-04 23:47:12 -0300

    creacion y edicion de usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,27 @@ class UsersController < ApplicationController
         end 
     end 
 
+    def edit 
+        @user = User.find(params[:id])
+    end 
+
+    def update 
+        @user = User.find(params[:id])
+
+        if @user.update(edit_params)
+            render new_user_path 
+        else :edit 
+        end 
+    end 
+
     private 
 
     def user_params 
         params.require(:user).permit(:username, :email, :password)
+    end 
+
+    def edit_params 
+        params.require(:user).permit(:username, :email)
     end 
 
 end
